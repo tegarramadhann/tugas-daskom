c299d60e6e54bc2b185572ee63120b2ea0f8afa2
tegarramadhann <119510350+tegarramadhann@users.noreply.github.com>
2022-12-23 23:29:55 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+list_angka = []
+
+funtion tambahangka(){
+    angka =
+    document.getElementById('angka').value
+    list_angka.push(angka)
+    document.getElementById('hasil').innerText=
+    list_angka
+}
